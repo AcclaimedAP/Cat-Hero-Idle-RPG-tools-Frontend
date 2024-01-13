fe00d4686fbcd4af6e9e5eed4ebfeeefe0822a24
AcclaimedAP <alexander.w.pressfelt@gmail.com>
2024-01-13 19:23:39 +0100

    Add base companion data to show structure.

---
+++ src/data/companions/companions.js
@@ -0,0 +1,55 @@
+export const companions = [
+  {
+    name: "Sausage",
+    tier: "Uncommon",
+    manaCost: 1,
+    baseAttack: 0.1,
+    baseASPD: 0.4,
+    attackPerLevel: 0.005,
+    ASPDPerLevel: 0.002,
+    specialEffect: [
+      {
+        bonusId: 0,
+        description: "Lv.${level}: Summons extra 1 sausages upon casting Dinner Time that deal extra ${sausageDmgMultiplier}% DMG of the Skill DMG.",
+        tiers: [
+          {
+            level: 1,
+            requiredLevel: 1,
+            value1: 1.5
+          },
+          {
+            level: 2,
+            requiredLevel: 21,
+            value1: 2.5
+          },
+          {
+            level: 3,
+            requiredLevel: 41,
+            value1: 5.5
+          }
+        ]
+      },
+      {
+        bonusId: 1,
+        description: "Lv.${level}: Speeds up the cooldown of Dinner Time by ${coolDownSpeed}%.",
+        tiers: [
+          {
+            level: 1,
+            requiredLevel: 11,
+            value1: 0.2
+          },
+          {
+            level: 2,
+            requiredLevel: 31,
+            value1: 0.4
+          },
+          {
+            level: 3,
+            requiredLevel: 51,
+            value1: 0.6
+          }
+        ]
+      }
+    ]
+  }
+]
