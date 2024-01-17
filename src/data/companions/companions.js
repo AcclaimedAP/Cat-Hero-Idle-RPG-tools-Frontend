20136a3b7ac466c2eaa8704d8102a3679b069c77
AcclaimedAP <alexander.w.pressfelt@gmail.com>
2024-01-17 21:09:24 +0100

    Add base object structures for all companions

--- src/data/companions/companions.js
+++ src/data/companions/companions.js
@@ -54,8 +54,957 @@ export const companions = [
         ]
       }
     ]
-  }
+  },
+  {
+    name: "Sunny",
+    rarity: "Uncommon",
+    image: null,
+    manaCost: 1,
+    baseAttack: 0.1,
+    baseASPD: 0.4,
+    attackPerLevel: 0.005,
+    ASPDPerLevel: 0.002,
+    specialEffect: [
+      {
+        bonusId: 0,
+        description: "",
+        tiers: [
+          {
+            level: 1,
+            requiredLevel: 1
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: "Bread",
+    rarity: "Uncommon",
+    image: null,
+    manaCost: 1,
+    baseAttack: 0.1,
+    baseASPD: 0.4,
+    attackPerLevel: 0.005,
+    ASPDPerLevel: 0.002,
+    specialEffect: [
+      {
+        bonusId: 0,
+        description: "",
+        tiers: [
+          {
+            level: 1,
+            requiredLevel: 1
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: "Cracker",
+    rarity: "Uncommon",
+    image: null,
+    manaCost: 2,
+    baseAttack: 0.1,
+    baseASPD: 0.4,
+    attackPerLevel: 0.005,
+    ASPDPerLevel: 0.002,
+    specialEffect: [
+      {
+        bonusId: 0,
+        description: "",
+        tiers: [
+          {
+            level: 1,
+            requiredLevel: 1
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: "Pudding",
+    rarity: "Uncommon",
+    image: null,
+    manaCost: 2,
+    baseAttack: 0.1,
+    baseASPD: 0.4,
+    attackPerLevel: 0.005,
+    ASPDPerLevel: 0.002,
+    specialEffect: [
+      {
+        bonusId: 0,
+        description: "",
+        tiers: [
+          {
+            level: 1,
+            requiredLevel: 1
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: "Cake",
+    rarity: "Uncommon",
+    image: null,
+    manaCost: 2,
+    baseAttack: 0.1,
+    baseASPD: 0.4,
+    attackPerLevel: 0.005,
+    ASPDPerLevel: 0.002,
+    specialEffect: [
+      {
+        bonusId: 0,
+        description: "",
+        tiers: [
+          {
+            level: 1,
+            requiredLevel: 1
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: "Tamago",
+    rarity: "Rare",
+    image: null,
+    manaCost: 3,
+    baseAttack: 0.1,
+    baseASPD: 0.4,
+    attackPerLevel: 0.005,
+    ASPDPerLevel: 0.002,
+    specialEffect: [
+      {
+        bonusId: 0,
+        description: "",
+        tiers: [
+          {
+            level: 1,
+            requiredLevel: 1
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: "Carrot",
+    rarity: "Rare",
+    image: null,
+    manaCost: 3,
+    baseAttack: 0.1,
+    baseASPD: 0.4,
+    attackPerLevel: 0.005,
+    ASPDPerLevel: 0.002,
+    specialEffect: [
+      {
+        bonusId: 0,
+        description: "",
+        tiers: [
+          {
+            level: 1,
+            requiredLevel: 1
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: "Clover",
+    rarity: "Rare",
+    image: null,
+    manaCost: 3,
+    baseAttack: 0.1,
+    baseASPD: 0.4,
+    attackPerLevel: 0.005,
+    ASPDPerLevel: 0.002,
+    specialEffect: [
+      {
+        bonusId: 0,
+        description: "",
+        tiers: [
+          {
+            level: 1,
+            requiredLevel: 1
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: "Trunk",
+    rarity: "Rare",
+    image: null,
+    manaCost: 3,
+    baseAttack: 0.1,
+    baseASPD: 0.4,
+    attackPerLevel: 0.005,
+    ASPDPerLevel: 0.002,
+    specialEffect: [
+      {
+        bonusId: 0,
+        description: "",
+        tiers: [
+          {
+            level: 1,
+            requiredLevel: 1
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: "Tacoyaki",
+    rarity: "Rare",
+    image: null,
+    manaCost: 3,
+    baseAttack: 0.1,
+    baseASPD: 0.4,
+    attackPerLevel: 0.005,
+    ASPDPerLevel: 0.002,
+    specialEffect: [
+      {
+        bonusId: 0,
+        description: "",
+        tiers: [
+          {
+            level: 1,
+            requiredLevel: 1
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: "Chick",
+    rarity: "Rare",
+    image: null,
+    manaCost: 3,
+    baseAttack: 0.1,
+    baseASPD: 0.4,
+    attackPerLevel: 0.005,
+    ASPDPerLevel: 0.002,
+    specialEffect: [
+      {
+        bonusId: 0,
+        description: "",
+        tiers: [
+          {
+            level: 1,
+            requiredLevel: 1
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: "Burger",
+    rarity: "Rare",
+    image: null,
+    manaCost: 3,
+    baseAttack: 0.1,
+    baseASPD: 0.4,
+    attackPerLevel: 0.005,
+    ASPDPerLevel: 0.002,
+    specialEffect: [
+      {
+        bonusId: 0,
+        description: "",
+        tiers: [
+          {
+            level: 1,
+            requiredLevel: 1
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: "Jackdolf",
+    rarity: "Epic",
+    image: null,
+    manaCost: 4,
+    baseAttack: 0.8,
+    baseASPD: 0.8,
+    attackPerLevel: 0.01,
+    ASPDPerLevel: 0.005,
+    specialEffect: [
+      {
+        bonusId: 0,
+        description: "",
+        tiers: [
+          {
+            level: 1,
+            requiredLevel: 1
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: "Mouse",
+    rarity: "Epic",
+    image: null,
+    manaCost: 4,
+    baseAttack: 0.8,
+    baseASPD: 0.8,
+    attackPerLevel: 0.01,
+    ASPDPerLevel: 0.005,
+    specialEffect: [
+      {
+        bonusId: 0,
+        description: "",
+        tiers: [
+          {
+            level: 1,
+            requiredLevel: 1
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: "Pingu",
+    rarity: "Epic",
+    image: null,
+    manaCost: 4,
+    baseAttack: 0.8,
+    baseASPD: 0.8,
+    attackPerLevel: 0.01,
+    ASPDPerLevel: 0.005,
+    specialEffect: [
+      {
+        bonusId: 0,
+        description: "",
+        tiers: [
+          {
+            level: 1,
+            requiredLevel: 1
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: "Ghost Cat",
+    rarity: "Epic",
+    image: null,
+    manaCost: 4,
+    baseAttack: 0.8,
+    baseASPD: 0.8,
+    attackPerLevel: 0.01,
+    ASPDPerLevel: 0.005,
+    specialEffect: [
+      {
+        bonusId: 0,
+        description: "",
+        tiers: [
+          {
+            level: 1,
+            requiredLevel: 1
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: "Bunny",
+    rarity: "Epic",
+    image: null,
+    manaCost: 4,
+    baseAttack: 0.8,
+    baseASPD: 0.8,
+    attackPerLevel: 0.01,
+    ASPDPerLevel: 0.005,
+    specialEffect: [
+      {
+        bonusId: 0,
+        description: "",
+        tiers: [
+          {
+            level: 1,
+            requiredLevel: 1
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: "Fire Chick",
+    rarity: "Epic",
+    image: null,
+    manaCost: 4,
+    baseAttack: 0.8,
+    baseASPD: 0.8,
+    attackPerLevel: 0.01,
+    ASPDPerLevel: 0.005,
+    specialEffect: [
+      {
+        bonusId: 0,
+        description: "",
+        tiers: [
+          {
+            level: 1,
+            requiredLevel: 1
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: "Tulip",
+    rarity: "Epic",
+    image: null,
+    manaCost: 4,
+    baseAttack: 0.8,
+    baseASPD: 0.8,
+    attackPerLevel: 0.01,
+    ASPDPerLevel: 0.005,
+    specialEffect: [
+      {
+        bonusId: 0,
+        description: "",
+        tiers: [
+          {
+            level: 1,
+            requiredLevel: 1
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: "Phoenix",
+    rarity: "Epic",
+    image: null,
+    manaCost: 4,
+    baseAttack: 0.8,
+    baseASPD: 0.8,
+    attackPerLevel: 0.01,
+    ASPDPerLevel: 0.005,
+    specialEffect: [
+      {
+        bonusId: 0,
+        description: "",
+        tiers: [
+          {
+            level: 1,
+            requiredLevel: 1
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: "Lightree",
+    rarity: "Epic",
+    image: null,
+    manaCost: 4,
+    baseAttack: 0.8,
+    baseASPD: 0.8,
+    attackPerLevel: 0.01,
+    ASPDPerLevel: 0.005,
+    specialEffect: [
+      {
+        bonusId: 0,
+        description: "",
+        tiers: [
+          {
+            level: 1,
+            requiredLevel: 1
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: "Whopper",
+    rarity: "Epic",
+    image: null,
+    manaCost: 4,
+    baseAttack: 0.8,
+    baseASPD: 0.8,
+    attackPerLevel: 0.01,
+    ASPDPerLevel: 0.005,
+    specialEffect: [
+      {
+        bonusId: 0,
+        description: "",
+        tiers: [
+          {
+            level: 1,
+            requiredLevel: 1
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: "Drake",
+    rarity: "Epic",
+    image: null,
+    manaCost: 4,
+    baseAttack: 0.8,
+    baseASPD: 0.8,
+    attackPerLevel: 0.01,
+    ASPDPerLevel: 0.005,
+    specialEffect: [
+      {
+        bonusId: 0,
+        description: "",
+        tiers: [
+          {
+            level: 1,
+            requiredLevel: 1
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: "Spark",
+    rarity: "Epic",
+    image: null,
+    manaCost: 4,
+    baseAttack: 0.8,
+    baseASPD: 0.8,
+    attackPerLevel: 0.01,
+    ASPDPerLevel: 0.005,
+    specialEffect: [
+      {
+        bonusId: 0,
+        description: "",
+        tiers: [
+          {
+            level: 1,
+            requiredLevel: 1
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: "Cookie Knight",
+    rarity: "Epic",
+    image: null,
+    manaCost: 4,
+    baseAttack: 0.8,
+    baseASPD: 0.8,
+    attackPerLevel: 0.01,
+    ASPDPerLevel: 0.005,
+    specialEffect: [
+      {
+        bonusId: 0,
+        description: "",
+        tiers: [
+          {
+            level: 1,
+            requiredLevel: 1
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: "Pumpky",
+    rarity: "Epic",
+    image: null,
+    manaCost: 4,
+    baseAttack: 0.8,
+    baseASPD: 0.8,
+    attackPerLevel: 0.01,
+    ASPDPerLevel: 0.005,
+    specialEffect: [
+      {
+        bonusId: 0,
+        description: "",
+        tiers: [
+          {
+            level: 1,
+            requiredLevel: 1
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: "Steel Fin",
+    rarity: "Epic",
+    image: null,
+    manaCost: 4,
+    baseAttack: 0.8,
+    baseASPD: 0.8,
+    attackPerLevel: 0.01,
+    ASPDPerLevel: 0.005,
+    specialEffect: [
+      {
+        bonusId: 0,
+        description: "",
+        tiers: [
+          {
+            level: 1,
+            requiredLevel: 1
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: "Angry Bomb",
+    rarity: "Epic",
+    image: null,
+    manaCost: 4,
+    baseAttack: 0.8,
+    baseASPD: 0.8,
+    attackPerLevel: 0.01,
+    ASPDPerLevel: 0.005,
+    specialEffect: [
+      {
+        bonusId: 0,
+        description: "",
+        tiers: [
+          {
+            level: 1,
+            requiredLevel: 1
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: "Devil Cat",
+    rarity: "Epic",
+    image: null,
+    manaCost: 4,
+    baseAttack: 0.8,
+    baseASPD: 0.8,
+    attackPerLevel: 0.01,
+    ASPDPerLevel: 0.005,
+    specialEffect: [
+      {
+        bonusId: 0,
+        description: "",
+        tiers: [
+          {
+            level: 1,
+            requiredLevel: 1
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: "Bro",
+    rarity: "Epic",
+    image: null,
+    manaCost: 4,
+    baseAttack: 0.8,
+    baseASPD: 0.8,
+    attackPerLevel: 0.01,
+    ASPDPerLevel: 0.005,
+    specialEffect: [
+      {
+        bonusId: 0,
+        description: "",
+        tiers: [
+          {
+            level: 1,
+            requiredLevel: 1
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: "Sparrow",
+    rarity: "Epic",
+    image: null,
+    manaCost: 4,
+    baseAttack: 0.8,
+    baseASPD: 0.8,
+    attackPerLevel: 0.01,
+    ASPDPerLevel: 0.005,
+    specialEffect: [
+      {
+        bonusId: 0,
+        description: "",
+        tiers: [
+          {
+            level: 1,
+            requiredLevel: 1
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: "Cat Black",
+    rarity: "Legendary",
+    image: null,
+    manaCost: 6,
+    baseAttack: 3.2,
+    baseASPD: 1,
+    attackPerLevel: 0.103,
+    ASPDPerLevel: 0.01,
+    specialEffect: [
+      {
+        bonusId: 0,
+        description: "",
+        tiers: [
+          {
+            level: 1,
+            requiredLevel: 1
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: "Jack Striker",
+    rarity: "Legendary",
+    image: null,
+    manaCost: 8,
+    baseAttack: 3.2,
+    baseASPD: 1,
+    attackPerLevel: 0.103,
+    ASPDPerLevel: 0.01,
+    specialEffect: [
+      {
+        bonusId: 0,
+        description: "",
+        tiers: [
+          {
+            level: 1,
+            requiredLevel: 1
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: "Werewolf",
+    rarity: "Legendary",
+    image: null,
+    manaCost: 8,
+    baseAttack: 3.2,
+    baseASPD: 1,
+    attackPerLevel: 0.103,
+    ASPDPerLevel: 0.01,
+    specialEffect: [
+      {
+        bonusId: 0,
+        description: "",
+        tiers: [
+          {
+            level: 1,
+            requiredLevel: 1
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: "Grom",
+    rarity: "Legendary",
+    image: null,
+    manaCost: 8,
+    baseAttack: 3.2,
+    baseASPD: 1,
+    attackPerLevel: 0.103,
+    ASPDPerLevel: 0.01,
+    specialEffect: [
+      {
+        bonusId: 0,
+        description: "",
+        tiers: [
+          {
+            level: 1,
+            requiredLevel: 1
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: "LuLu",
+    rarity: "Legendary",
+    image: null,
+    manaCost: 8,
+    baseAttack: 3.2,
+    baseASPD: 1,
+    attackPerLevel: 0.103,
+    ASPDPerLevel: 0.01,
+    specialEffect: [
+      {
+        bonusId: 0,
+        description: "",
+        tiers: [
+          {
+            level: 1,
+            requiredLevel: 1
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: "RoRo",
+    rarity: "Legendary",
+    image: null,
+    manaCost: 6,
+    baseAttack: 3.2,
+    baseASPD: 1,
+    attackPerLevel: 0.103,
+    ASPDPerLevel: 0.01,
+    specialEffect: [
+      {
+        bonusId: 0,
+        description: "",
+        tiers: [
+          {
+            level: 1,
+            requiredLevel: 1
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: "Jack Tiger",
+    rarity: "Legendary",
+    image: null,
+    manaCost: 6,
+    baseAttack: 3.2,
+    baseASPD: 1,
+    attackPerLevel: 0.103,
+    ASPDPerLevel: 0.01,
+    specialEffect: [
+      {
+        bonusId: 0,
+        description: "",
+        tiers: [
+          {
+            level: 1,
+            requiredLevel: 1
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: "Jack Jaws",
+    rarity: "Legendary",
+    image: null,
+    manaCost: 6,
+    baseAttack: 3.2,
+    baseASPD: 1,
+    attackPerLevel: 0.103,
+    ASPDPerLevel: 0.01,
+    specialEffect: [
+      {
+        bonusId: 0,
+        description: "",
+        tiers: [
+          {
+            level: 1,
+            requiredLevel: 1
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: "Jack Fly",
+    rarity: "Legendary",
+    image: null,
+    manaCost: 6,
+    baseAttack: 3.2,
+    baseASPD: 1,
+    attackPerLevel: 0.103,
+    ASPDPerLevel: 0.01,
+    specialEffect: [
+      {
+        bonusId: 0,
+        description: "",
+        tiers: [
+          {
+            level: 1,
+            requiredLevel: 1
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: "War Wolf",
+    rarity: "Legendary",
+    image: null,
+    manaCost: 6,
+    baseAttack: 3.2,
+    baseASPD: 1,
+    attackPerLevel: 0.103,
+    ASPDPerLevel: 0.01,
+    specialEffect: [
+      {
+        bonusId: 0,
+        description: "",
+        tiers: [
+          {
+            level: 1,
+            requiredLevel: 1
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: "Jackdaw",
+    rarity: "Legendary",
+    image: null,
+    manaCost: 8,
+    baseAttack: 3.2,
+    baseASPD: 1,
+    attackPerLevel: 0.103,
+    ASPDPerLevel: 0.01,
+    specialEffect: [
+      {
+        bonusId: 0,
+        description: "",
+        tiers: [
+          {
+            level: 1,
+            requiredLevel: 1
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: "Tanker",
+    rarity: "Legendary",
+    image: null,
+    manaCost: 8,
+    baseAttack: 3.2,
+    baseASPD: 1,
+    attackPerLevel: 0.103,
+    ASPDPerLevel: 0.01,
+    specialEffect: [
+      {
+        bonusId: 0,
+        description: "",
+        tiers: [
+          {
+            level: 1,
+            requiredLevel: 1
+          }
+        ]
+      }
+    ]
+  },
 ]
+
+
+
 function getDescription(level = 21) {
   const effects = []
   this.specialEffect.map(effect => {
