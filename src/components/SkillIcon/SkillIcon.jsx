478184838d97de583075864950ad67c9606a1b44
AcclaimedAP <alexander.w.pressfelt@gmail.com>
2024-01-17 23:30:19 +0100

    Add base data for all skills

--- src/components/SkillIcon/SkillIcon.jsx
+++ src/components/SkillIcon/SkillIcon.jsx
@@ -4,7 +4,7 @@ export const SkillIcon = ({ skill, label = true, level = 1 }) => {
 	const background = getIconBackground(skill.rarity);
 	return (
 		<>
-			<div className='relative w-12 h-12'>
+			<div className='relative w-12 h-12 inline-block m-1'>
 				{label && <span className='absolute z-20 text-[0.6rem] left-[4px] top-[2px]'>Lv. {level}</span>}
 				<img
 					className='absolute z-10 left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[45%] h-[45%]'
