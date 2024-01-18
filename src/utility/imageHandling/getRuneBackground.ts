07d954c78a984793af74b244be620754d658a9b4
AcclaimedAP <alexander.w.pressfelt@gmail.com>
2024-01-18 14:27:42 +0100

    Add basic data for all Sub runes

---
+++ src/utility/imageHandling/getRuneBackground.ts
@@ -0,0 +1,47 @@
+import BgMainUncommon from '../../assets/images/runes/mega-runes/backgrounds/bg_main_uncommon.png';
+import BgMainRare from '../../assets/images/runes/mega-runes/backgrounds/bg_main_rare.png';
+import BgMainEpic from '../../assets/images/runes/mega-runes/backgrounds/bg_main_epic.png';
+import BgMainLegendary from '../../assets/images/runes/mega-runes/backgrounds/bg_main_legendary.png';
+import BgMainMythic from '../../assets/images/runes/mega-runes/backgrounds/bg_main_mythic.png';
+import BgSubUncommon from '../../assets/images/runes/mega-runes/backgrounds/bg_sub_uncommon.png';
+import BgSubRare from '../../assets/images/runes/mega-runes/backgrounds/bg_sub_rare.png';
+import BgSubEpic from '../../assets/images/runes/mega-runes/backgrounds/bg_sub_epic.png';
+import BgSubLegendary from '../../assets/images/runes/mega-runes/backgrounds/bg_sub_legendary.png';
+import BgSubMythic from '../../assets/images/runes/mega-runes/backgrounds/bg_sub_mythic.png';
+
+export const getRuneBackground = (type: string, rarity: string) => {
+	switch (type) {
+		case 'Main':
+			switch (rarity) {
+				case 'Uncommon':
+					return BgMainUncommon;
+				case 'Rare':
+					return BgMainRare;
+				case 'Epic':
+					return BgMainEpic;
+				case 'Legendary':
+					return BgMainLegendary;
+				case 'Mythic':
+					return BgMainMythic;
+				default:
+					return BgMainUncommon;
+			}
+		case 'Sub':
+			switch (rarity) {
+				case 'Uncommon':
+					return BgSubUncommon;
+				case 'Rare':
+					return BgSubRare;
+				case 'Epic':
+					return BgSubEpic;
+				case 'Legendary':
+					return BgSubLegendary;
+				case 'Mythic':
+					return BgSubMythic;
+				default:
+					return BgSubUncommon;
+			}
+		default:
+			return BgMainUncommon;
+	}
+};
