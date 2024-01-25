b58f2f22fdcf46e53297259f01ed23273db664ab
AcclaimedAP <alexander.w.pressfelt@gmail.com>
2024-01-25 17:42:56 +0100

    Fix config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: '/Cat-Hero-Idle-RPG-tools-Frontend/',
 	plugins: [react()],
 	resolve: {
 		alias: {
