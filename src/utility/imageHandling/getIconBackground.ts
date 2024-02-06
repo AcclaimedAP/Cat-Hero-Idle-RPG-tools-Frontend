eb578d9230b7553a759b7ab5490208e3d04207d9
AcclaimedAP <alexander.w.pressfelt@gmail.com>
2024-02-06 17:02:36 +0100

    Setup Typedoc for documentations of functions and code

--- src/utility/imageHandling/getIconBackground.ts
+++ src/utility/imageHandling/getIconBackground.ts
@@ -4,6 +4,15 @@ import ImageBackgroundRare from 'assets/sprites/background/GV_ui_main19_2.png';
 import ImageBackgroundEpic from 'assets/sprites/background/GV_ui_main19_3.png';
 import ImageBackgroundLegendary from 'assets/sprites/background/GV_ui_main19_4.png';
 import ImageBackgroundMythic from 'assets/sprites/background/GV_ui_main19_5.png';
+/**
+ * Get the background image for the icon based on the rarity
+ * @param rarity - The rarity of the icon
+ * @returns The background image for the icon
+ * @example
+ * const background = getIconBackground('Common');
+ * // returns ImageBackgroundCommon
+ * 
+ */
 
 export const getIconBackground = (rarity: string) => {
 	switch (rarity) {
