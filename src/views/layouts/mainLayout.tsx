6fcd5a5fef82a4d2d6e228471673afcec8e39f2c
AcclaimedAP <alexander.w.pressfelt@gmail.com>
2024-07-09 18:02:40 +0200

    feat(coffee) Add buymeacoffee widget for leeauf

--- src/views/layouts/mainLayout.tsx
+++ src/views/layouts/mainLayout.tsx
@@ -1,22 +1,25 @@
 import { Outlet } from 'react-router-dom';
 import { NavigationBar } from 'components/NavigationBar/NavigationBar';
+import Buymeacoffee from './buymecoffee';
 
 export const Layout = () => {
-	return (
-		<>
-			<header>
-				<NavigationBar />
-			</header>
+
+  return (
+    <>
+      <header id='header'>
+        <NavigationBar />
+        <Buymeacoffee />
+      </header>
       <main className='py-6 md:p-12 flex flex-row justify-center w-full'>
         <div className='max-w-[1090px] w-full overflow-x-hidden md:overflow-x-visible'>
           <Outlet></Outlet>
         </div>
-			</main>
+      </main>
       <footer className='md:px-12 mb-[120px]'></footer>
-			<div className='background-image'>
-				<div className='background-image-front'></div>
-				<div className='background-image-front-two'></div>
-			</div>
-		</>
-	);
+      <div className='background-image'>
+        <div className='background-image-front'></div>
+        <div className='background-image-front-two'></div>
+      </div>
+    </>
+  );
 };
