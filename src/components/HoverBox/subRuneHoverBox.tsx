c64b4f31ab74b8c63b930eb0f4653523f47164ac
Alexander Pressfelt <alexander.w.pressfelt@gmail.com>
2025-01-18 16:21:51 +0100

    feat/builder: Add description color parsing functionality  (#133)
    * feat/builder: Add description color parsing functionality
    
    * feat/builder: Update styling for hoverboxes to adapt to the size of the content a bit better
    
    * feat/builder: Add dynamic height to sub runes
    
    * feat/fix: Remove debugging

--- src/components/HoverBox/subRuneHoverBox.tsx
+++ src/components/HoverBox/subRuneHoverBox.tsx
@@ -5,7 +5,7 @@ import { RuneIcon } from "../RuneIcon/RuneIcon";
 import { getDeviceType } from "src/utility/device/getDevice";
 import { IHoverBox } from "src/types/IHoverBox";
 import Game from "types/game";
-
+import { DescriptionText } from "./DescriptionText";
 /**
  * Hover box for the sub rune
  * @param {ISubRune} rune - The sub rune object
@@ -26,6 +26,22 @@ export const HoverBox = forwardRef(({ rune }: { rune: Game.Rune.SubRune }, ref: 
       setIsVisible(false);
     }
   }));
+  const heightValue = rune.description.length + ((rune.description.split('\n').length - 1) * 30);
+  const heightClass = function (height: number) {
+    if (height < 65) {
+      return "h-32";
+    } else if (height < 85) {
+      return "h-36";
+    } else if (height < 100) {
+      return "h-40";
+    } else if (height < 120) {
+      return "h-44";
+    } else if (height < 140) {
+      return "h-48";
+    } else {
+      return "h-52";
+    }
+  }
 
   if (!isVisible) {
     return (
@@ -34,7 +50,7 @@ export const HoverBox = forwardRef(({ rune }: { rune: Game.Rune.SubRune }, ref: 
   }
   return (
     <HoverContainer type="sub-rune">
-      <div className='hover-box hover-box-sub-rune flex justify-center items-start h-36 w-56' onMouseOver={() => { clearTimeout(hoverTimeout); setIsVisible(false); }}>
+      <div className={`hover-box hover-box-sub-rune flex justify-center items-start ${heightClass(heightValue)} w-56`} onMouseOver={() => { clearTimeout(hoverTimeout); setIsVisible(false); }}>
         <Ribbon width={""}>{rune.name}</Ribbon>
         <div className='flex flex-row justify-between items-start translate-y-12 mt-1 gap-2 w-48'>
           <div>
@@ -42,7 +58,7 @@ export const HoverBox = forwardRef(({ rune }: { rune: Game.Rune.SubRune }, ref: 
           </div>
           <div className="flex flex-col">
             <div className="text-[0.6rem] p-2 m-1 mb-2 rounded-md hover-type">
-              <span>{rune.description}</span>
+              <span><DescriptionText description={rune.description} /></span>
             </div>
           </div>
         </div>
