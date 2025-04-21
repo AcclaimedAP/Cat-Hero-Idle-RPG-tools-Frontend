8008e68e1b6e9da868636bcf8a5ede045bd0218b
AcclaimedAP <alexander.w.pressfelt@gmail.com>
2025-04-21 14:38:29 +0200

    Remove buymecoffee, remove GA tracking, etc

--- src/views/layouts/mainLayout.tsx
+++ src/views/layouts/mainLayout.tsx
@@ -1,6 +1,5 @@
 import { Outlet } from 'react-router-dom';
 import { NavigationBar } from 'components/NavigationBar/NavigationBar';
-import Buymeacoffee from './buymecoffee';
 import { Cookies } from 'react-cookie-consent';
 export const Layout = () => {
   const resetCookieConsent = () => {
@@ -13,7 +12,6 @@ export const Layout = () => {
 
       <header id='header'>
         <NavigationBar />
-        <Buymeacoffee />
       </header>
       <div className='flex flex-col flex-nowrap justify-between min-h-screen'>
         <main className='py-6 md:p-12 flex flex-row justify-center w-full'>
@@ -24,9 +22,12 @@ export const Layout = () => {
         <footer className='md:px-12 mb-[120px] flex flex-row justify-center w-full'>
           <div className='max-w-[1090px] '>
             <div className='container-dark text-xs  overflow-x-hidden md:overflow-x-visible'>
-              <p className='px-12 py-4'>The site uses cookies to log the amount of traffic, however because of the technical limitations of GitHub Pages, I am limited to using things like Google Analytics to get this type of statistics. Even with all types of options turned off, some data will be collected if you accepted the cookies. If you choose to change your choice, please click here: <button className='hover:drop-shadow-md underline'
+              <p className='px-12 py-4'>
+                {/*The site uses cookies to log the amount of traffic, however because of the technical limitations of GitHub Pages, I am limited to using things like Google Analytics to get this type of statistics. Even with all types of options turned off, some data will be collected if you accepted the cookies. If you choose to change your choice, please click here: <button className='hover:drop-shadow-md underline'
                 onClick={() => resetCookieConsent()}
-              >Reset cookies</button>. Please note this refreshes the page. Ensure you have saved any build you are working on.</p>
+              >Reset cookies</button>. Please note this refreshes the page. Ensure you have saved any build you are working on.*/}
+                The site uses PostHog to track website usage. The data it collects is fully anonymous and cannot be used to identify you. It only logs the page the user visits, with no additional information. If you have any questions or concerns, please react out to me on Discord.
+              </p>
             </div>
           </div>
         </footer>
